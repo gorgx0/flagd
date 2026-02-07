6c14e9dec07ba1d5c6bab4f185ef1099eff1c7c0
Marcin Krasowski <marcin.krasowski@gmail.com>
2026-02-07 14:17:20 +0100

    wip

---
+++ app01/src/main/kotlin/gorg/home/sb01/Sb01Application.kt
@@ -0,0 +1,11 @@
+package gorg.home.sb01
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sb01Application
+
+fun main(args: Array<String>) {
+	runApplication<Sb01Application>(*args)
+}
