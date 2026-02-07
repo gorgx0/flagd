177a9105f5cdb55d0ba0fecfddfedbe7b2b8ab6c
Marcin Krasowski <marcin.krasowski@gmail.com>
2026-02-07 17:30:42 +0100

    wip

--- app01/src/main/kotlin/gorg/home/sb01/Sb01Application.kt
+++ app01/src/main/kotlin/gorg/home/sb01/Sb01Application.kt
@@ -1,11 +1,20 @@
 package gorg.home.sb01
 
+import dev.openfeature.sdk.OpenFeatureAPI
+import dev.openfeature.contrib.providers.flagd.FlagdProvider
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ConfigurableApplicationContext
 
 @SpringBootApplication
 class Sb01Application
 
 fun main(args: Array<String>) {
-	runApplication<Sb01Application>(*args)
+	val context: ConfigurableApplicationContext = runApplication<Sb01Application>(*args)
+	val api = OpenFeatureAPI.getInstance()
+	api.setProvider(FlagdProvider())
+
+	val client = api.getClient()
+	val myNewFeature = client.getBooleanValue("my-new-feature", false)
+	println("my-new-feature: $myNewFeature")
 }
