ee58ca9b699540acd316311cec32be29a8f6293d
Marcin Krasowski <marcin.krasowski@gmail.com>
2026-02-08 13:19:27 +0100

    app01/config

--- app01/src/main/kotlin/gorg/home/sb01/Config.kt
+++ app01/src/main/kotlin/gorg/home/sb01/Config.kt
@@ -5,19 +5,29 @@ import dev.openfeature.contrib.providers.flagd.FlagdOptions
 import dev.openfeature.contrib.providers.flagd.FlagdProvider
 import dev.openfeature.sdk.Client
 import dev.openfeature.sdk.OpenFeatureAPI
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class Config {
+    @Value("\${openfeature.flagd.host:localhost}")
+    lateinit var host: String
+
+    @Value("\${openfeature.flagd.port:8013}")
+    var port: Int = 8013
+
+    @Value("\${openfeature.flagd.resolver-type:RPC}")
+    lateinit var resolverType: String
+
     @Bean
     fun client(): Client? {
         val api = OpenFeatureAPI.getInstance()
         api.setProviderAndWait(FlagdProvider(
             FlagdOptions.builder()
-                .host("flagd.localhost")
-                .port(30080)
-                .resolverType(Config.Resolver.RPC)
+                .host(host)
+                .port(port)
+                .resolverType(Config.Resolver.valueOf(resolverType))
                 .build()
         ))
         return api.getClient()
