2ae0da3121b6febb09afdaa472121e3746211d8c
Marcin Krasowski <marcin.krasowski@gmail.com>
2026-02-08 13:36:48 +0100

    app01/more_flags

--- app01/src/main/kotlin/gorg/home/sb01/IndexController.kt
+++ app01/src/main/kotlin/gorg/home/sb01/IndexController.kt
@@ -1,13 +1,16 @@
 package gorg.home.sb01
 
+import dev.openfeature.sdk.Client
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class IndexController {
+class IndexController(private val client: Client) {
 
     @GetMapping("/")
-    fun index(): String {
+    fun index(model: Model): String {
+        model.addAttribute("client", client)
         return "index"
     }
 
